337e4ae8e159b4eb53bcd81f872566fa904976bc
lkhalil <lkhalil@ncsu.edu>
2020-07-24 19:11:41 -0400

    update data path

--- gbpy/tests/test_ov.py
+++ gbpy/tests/test_ov.py
@@ -3,7 +3,7 @@ from gbpy.util_funcs import compute_ovito_data
 
 
 def test_compute_ovito_data():
-    filename0 = "./data/dump_1"
+    filename0 = "gbpy/tests/data/dump_1"
     data = compute_ovito_data(filename0)
     actual = 5765
     expected = data.particles.count
