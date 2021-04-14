858bc6d711fa6bb70e9e38309cef6f42751434ca
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2021-04-14 21:23:25 +0000

    Update docs after building Travis build 107 of spatala/gbpy
    The docs were built from the branch 'master' against the commit
    9a069765287c67cf8a866cb182e9ec5534686090.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/spatala/gbpy/jobs/767081640.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --built-docs docs/build/html .

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.post51+g34c14fe',
+    VERSION: '0.post51+g9a06976',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
