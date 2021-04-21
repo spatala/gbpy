b96cf18580c2a6f4436180b2a25720b4f2c834cd
lkhalil <lkhalil@ncsu.edu>
2021-04-21 10:44:54 -0400

    fix test for create_img

--- gbpy/tests/test_create_imgs.py
+++ gbpy/tests/test_create_imgs.py
@@ -3,16 +3,17 @@ import numpy as np
 import pytest
 import gbpy.pad_dump_file as pdf
 import gbpy.util_funcs as uf
+import byxtal.lattice as gbl
 
-
-@pytest.mark.parametrize('filename0, rCut, lat_par, non_p',
-                         [("gbpy/tests/data/dump_1", 8.1, 4.05, 2),
-                          ("gbpy/tests/data/dump_1", 30, 4.05, 2),
-                          ("gbpy/tests/data/dump_2", 8.1, 4.05, 1)])
-def test_create_imgs(filename0, rCut, lat_par, non_p):
+@pytest.mark.parametrize('filename0, rCut, element, non_p',
+                         [("tests/data/dump_1", 8.1, "Al", 2),
+                          ("tests/data/dump_1", 30, "Al", 2),
+                          ("tests/data/dump_2", 8.1, "Al", 1)])
+def test_create_imgs(filename0, rCut, element,  non_p):
+    l1 = gbl.Lattice(str(element))
     data = uf.compute_ovito_data(filename0)
     arr = pdf.p_arr(non_p)
-    GbRegion, GbIndex, GbWidth, w_bottom_SC, w_top_SC = pdf.GB_finder(data, lat_par, non_p, 'ptm', .1)
+    GbRegion, GbIndex, GbWidth, w_bottom_SC, w_top_SC = pdf.GB_finder(data, l1, non_p, 'ptm', .1)
     sim_cell = data.cell[...]
     sim_1vec = np.array(sim_cell[:, arr[0]])
     sim_2vec = np.array(sim_cell[:, arr[1]])
