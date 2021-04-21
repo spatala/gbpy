755bc4768941f656d55fc7e44c972d012ed8cfc6
lkhalil <lkhalil@ncsu.edu>
2021-04-21 10:55:33 -0400

    fix test

--- gbpy/tests/test_GB_finder.py
+++ gbpy/tests/test_GB_finder.py
@@ -2,18 +2,19 @@ import numpy as np
 import pytest
 import gbpy.pad_dump_file as pad
 import gbpy.util_funcs as uf
+import byxtal.lattice as gbl
 
-
-@pytest.mark.parametrize('filename0, lat_par, num_GBregion, actual_min_z_gbreg, actual_max_z_gbreg,'
+@pytest.mark.parametrize('filename0, element, num_GBregion, actual_min_z_gbreg, actual_max_z_gbreg,'
                          'actual_w_bottom_SC, actual_w_top_SC',
-                         [("gbpy/tests/data/dump_1", 4.05, 138, -2.811127714, 2.811127714, 94, 91.5),
-                          ("gbpy/tests/data/dump_2", 4.05, 51, -3.06795, 1.44512, 116.85, 118.462)])
-
-def test_GB_finder(filename0, lat_par, num_GBregion, actual_min_z_gbreg, actual_max_z_gbreg,
+                         [("tests/data/dump_2", "Al", 51, -3.06795, 1.44512, 116.85, 118.462)])
+# ("tests/data/dump_1", "Al", 138, -2.811127714, 2.811127714, 94, 91.5),
+                          
+def test_GB_finder(filename0, element, num_GBregion, actual_min_z_gbreg, actual_max_z_gbreg,
                    actual_w_bottom_SC, actual_w_top_SC):
+    l1 = gbl.Lattice(str(element))
     data = uf.compute_ovito_data(filename0)
     non_p = uf.identify_pbc(data)
-    GbRegion, GbIndex, GbWidth, w_bottom_SC, w_top_SC = pad.GB_finder(data, lat_par, non_p, 'ptm', '.1')
+    GbRegion, GbIndex, GbWidth, w_bottom_SC, w_top_SC = pad.GB_finder(data, l1, non_p, 'ptm', '.1')
 
     assert np.abs((actual_w_bottom_SC - w_bottom_SC)/actual_w_bottom_SC) < .5
     assert np.abs((actual_w_top_SC - w_top_SC)/actual_w_top_SC) < .5
